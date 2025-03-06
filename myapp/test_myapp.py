85163f9b1fec6d5d5de562a7f4b613f0276b2180
silkiqw <152535327+silkiqw@users.noreply.github.com>
2025-03-07 00:50:48 +0100

    Update test_myapp.py

--- myapp/test_myapp.py
+++ myapp/test_myapp.py
@@ -14,6 +14,7 @@ from unittest.mock import patch, mock_open
 from django.conf import settings
 from django.test import RequestFactory
 from myapp.views import (
+    distance
     is_within_rad,
     filter_duplicates,
     remove_duplicates,
