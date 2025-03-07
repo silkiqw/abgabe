4c2c40c00d5d4d0b5fba40247cae8edbb2805788
silkiqw <152535327+silkiqw@users.noreply.github.com>
2025-03-07 07:22:15 +0100

    Update test_myapp.py

--- myapp/test_myapp.py
+++ myapp/test_myapp.py
@@ -197,7 +197,7 @@ def test_check_within_radius(mock_get_names, mock_remove_duplicates, mock_filter
     assert context['result'] == [["ID1", "TestStation"]]
 
 @patch("django.shortcuts.render")
-@patch("myapp.views.read_stations", return_value="ID1 50.0 10.0 TMAX 2000 2020\nID2 60.0 20.0 TMIN 2001 2021")
+@patch("myapp.views.read_stations", return_value="ID1        50.0      10.0      TMAX 2000 2020\nID2        60.0      20.0      TMIN 2001 2021")
 @patch("myapp.views.is_within_rad", return_value=False)
 def test_check_outside_radius(mock_is_within_rad, mock_read_stations, mock_render):
     # Configure mock_render to return a response with context
