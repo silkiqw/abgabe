417b744a5f58d402e624beaedcc9f65e6dbd294e
guetzel <el-wald@web.de>
2025-03-12 21:28:43 +0100

    Update apps.py

--- myapp/apps.py
+++ myapp/apps.py
@@ -6,5 +6,9 @@ class MyappConfig(AppConfig):
     name = 'myapp'
 
     def ready(self):
+        #Verhindert doppeltes Ausführen beim Start
+        if os.environ.get("RUN_MAIN") != "true":
+            return  # Beendet die Funktion beim zweiten Aufruf
+        #Lädt Startet Download der NOOA-DAten beim Start
         from .data_loader import download_stations
         download_stations()
