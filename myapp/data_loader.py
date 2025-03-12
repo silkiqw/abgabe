08a03f1e07c4e0934f528d27f7e7fe1abf95ee69
guetzel <el-wald@web.de>
2025-03-12 21:27:26 +0100

    Update data_loader.py

--- myapp/data_loader.py
+++ myapp/data_loader.py
@@ -2,20 +2,32 @@ import requests
 import os
 from django.conf import settings
 
-URL = "https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd-inventory.txt"
-SAVE_PATH = os.path.join(settings.BASE_DIR, "data", "stations.txt")
-
+URL_inventory = "https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd-inventory.txt"
+URL_stations = "https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd-stations.csv"
+save_path_inventory = os.path.join(settings.BASE_DIR, "data", "stations.txt")
+save_path_csv = URL_STATIONS = os.path.join(settings.BASE_DIR, "data", "ghcnd-stations.csv")
 
 def download_stations():
-    print("Lade NOAA-Daten herunter...")
+    #Lädt die ghcnd-inventory.txt für die Angabe der Zeiträume für TMIN und TMAX sowie due ghcnd-stations.csv für die Namen der Stationen herunter und speichert diese
     try:
-        response = requests.get(URL, timeout=10)
+        print("Lade Inventory")
+        response = requests.get(URL_inventory, timeout=10)
         response.raise_for_status()  # Löst eine Exception bei HTTP-Fehlern aus
-        print(response.text[:100])
-        lines = [line.rstrip() + "\n" for line in response.text.split("\n") if "TMIN" in line or "TMAX" in line]
-        with open(SAVE_PATH, "w", encoding="utf-8") as file:
-            file.writelines(lines)
+        with open(save_path_inventory, "w", encoding="utf-8") as file:
+            file.write(response.text)
 
         print("Daten erfolgreich heruntergeladen und gespeichert.")
     except requests.RequestException as e:
         print(f"Fehler beim Laden der Daten: {e}")
+    
+    try:
+        print("Lade CSV")
+        response2 = requests.get(URL_stations, timeout=10)
+        response2.raise_for_status()  # Fehler werfen, falls Request fehlschlägt
+
+        with open(save_path_csv, "w", encoding="utf-8") as file:
+            file.write(response2.text)
+
+        print(f"Datei gespeichert unter: {save_path_csv}")
+    except requests.RequestException as e:
+        print(f"Fehler beim Laden von {URL_stations}: {e}")
