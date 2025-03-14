045a2ab35a20468e2aae6fa0cd8cdc9c5e04790f
guetzel <el-wald@web.de>
2025-03-14 01:09:24 +0100

    Update data_loader.py

--- myapp/data_loader.py
+++ myapp/data_loader.py
@@ -8,11 +8,11 @@ save_path_inventory = os.path.join(settings.BASE_DIR, "data", "stations.txt")
 save_path_csv = URL_STATIONS = os.path.join(settings.BASE_DIR, "data", "ghcnd-stations.csv")
 
 def download_stations():
-    #Lädt die ghcnd-inventory.txt für die Angabe der Zeiträume für TMIN und TMAX sowie due ghcnd-stations.csv für die Namen der Stationen herunter und speichert diese
+    #downloads and saves ghcnd-inventory.txt (Coordinates ans TMIN, TMAX) and ghcnd-stations.csv (station-names)
     try:
         print("Lade Inventory")
         response = requests.get(URL_inventory, timeout=10)
-        response.raise_for_status()  # Löst eine Exception bei HTTP-Fehlern aus
+        response.raise_for_status()  #Exception HTTP-error
         with open(save_path_inventory, "w", encoding="utf-8") as file:
             file.write(response.text)
 
@@ -23,7 +23,7 @@ def download_stations():
     try:
         print("Lade CSV")
         response2 = requests.get(URL_stations, timeout=10)
-        response2.raise_for_status()  # Fehler werfen, falls Request fehlschlägt
+        response2.raise_for_status()  
 
         with open(save_path_csv, "w", encoding="utf-8") as file:
             file.write(response2.text)
